153909b66ee31d396380f109f23b2879f3bd4d72
silmin_ <silvestmint@gmail.com>
2019-02-16 10:51:19 +0000

    add: out log

--- main.py
+++ main.py
@@ -6,6 +6,8 @@ from search_word import searchWord
 from get_userid import getUserids
 from block_user import blockUsers
 
+import datetime
+
 # from keys.py
 CK = keys.CONSUMER_KEY
 CS = keys.CONSUMER_SECRET
@@ -20,8 +22,12 @@ result = searchWord(twitter, "url:neta+ru.com exclude:retweets", 100)
 
 if result != [] : 
     user_ids = getUserids(result)
-    print(user_ids)
-    
     blockUsers(twitter, user_ids)
+
+    print("-----------------------------")
+    print(datetime.date.today())
+    print(user_ids)
 else :
+    print("-----------------------------")
+    print(datetime.date.today())
     print("error")
